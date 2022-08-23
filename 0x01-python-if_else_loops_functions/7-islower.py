f929eb8d81b33c557666368f96dc909d509cb055
Trevoland <104766934+Trevoland@users.noreply.github.com>
2022-08-23 12:37:21 +0200

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def islow(c)
-ascii_num >= 97 and ascii_num <= 122:
-    return True
-return False
+def islower(c):
+    ascii_num = ord(c)
+    if ascii_num >= 97 and ascii_num <= 122:
+        return True
+    return False
