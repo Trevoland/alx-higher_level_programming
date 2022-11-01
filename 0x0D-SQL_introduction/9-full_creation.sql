94f2bb5ac644d33744512061252df657321b9cfb
Trevoland Nagoor <trevolandn26@gmail.com>
2022-11-01 01:17:51 -0700

    task

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Script that creates a second_table in the database hbtn_0c_0
+-- and add multiples rows
+CREATE TABLE IF NOT EXISTS second_table
+(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table VALUES
+
